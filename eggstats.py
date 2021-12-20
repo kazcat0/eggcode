ec3f5d2488363e65d0f5ed3f028de5f39fcd1d05
kazcat0 <96318487+kazcat0@users.noreply.github.com>
2021-12-19 22:42:39 -0700

    better eggstats.py

--- eggstats.py
+++ eggstats.py
@@ -1,18 +1,21 @@
-#species needs are connected to their needed moisture levels
-#aquatic species need to be submerged in the bathtub, average species need no special considerations, and dry species need to be out in the hot sun
+"""species needs are connected to their needed moisture levels
 
-#shrimps, crabs, and octopi are aquatic species. birds, echidnas, and platypi are average species. dragons, insects, and reptiles are dry species.
+   aquatic species need to be submerged in the bathtub, average species need no 
+   special considerations, and dry species need to be out in the hot sun
+
+   shrimps, crabs, and octopi are aquatic species. birds, echidnas, and platypi 
+   are average species. dragons, insects, and reptiles are dry species."""
 
 import random
 
 def speciesSelect():
-	species = ["shrimp", "crab", "octopus", 
-				"bird", "echidna", "platypi", 
-				"dragon", "insect", "reptile"]
+    species = ["shrimp", "crab", "octopus", 
+                "bird", "echidna", "platypi", 
+                "dragon", "insect", "reptile"]
 
-	select = species[random.randrange(0,9)]
+    select = species[random.randrange(0,len(species))]
 
-	print(f"Your species is {select}.")
+    print(f"Your species is {select}.")
 
 speciesSelect()
 
